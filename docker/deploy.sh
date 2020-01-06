03df642c818d4db00c39581e177bcf0a6f8d174b
Mateo Tutic <mateo@tutic.ch>
2020-01-06 15:15:43 +0100

    update deploy script

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,1 +1,29 @@
-echo "Hello world"
+echo "----- Deploying -----"
+
+# Check for needed software
+echo "----- Download openssh -----"
+apt-get install openssh
+
+# SSH config
+echo "----- SSH Config -----"
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
+eval "$(ssh-agent -s)"
+echo "$PROD_SERVER_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
+
+ssh-keyscan -H $PROD_HOSTNAME >> ~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
+
+# Commands in remote host
+echo "----- Setup vars, run updated containers -----"
+ssh -T $PROD_SERVER_USER@$PROD_HOSTNAME << EOF
+    cd /MACBot
+
+    # Update sources
+    git pull origin master
+
+    # Update containers with minimal downtime
+    docker-compose down
+	  docker-compose up --build -d
+EOF
