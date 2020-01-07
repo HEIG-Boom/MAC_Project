f159d86fa8d9852c33ce1f8fe784e3bd0cca5864
Mateo Tutic <mateo@tutic.ch>
2020-01-07 11:32:56 +0100

    Update deploy script

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,12 +3,8 @@ echo "----- Deploying -----"
 echo "----- Setup vars, run updated containers -----"
 cd /MACBot/docker
 
-rm env-file
-
 # Update sources
 git pull origin master
-
-echo "TELEGRAM_TOKEN=$TELEGRAM_TOKEN" > env-file
 
 # Update containers with minimal downtime
 docker-compose down
