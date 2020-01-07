23419d52cad2f8a392a0f5d1421dd2c4e74061d5
Laykel <layke.luc@gmail.com>
2020-01-07 11:12:21 +0100

    Set telegram token in pipeline

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -3,9 +3,13 @@ echo "----- Deploying -----"
 echo "----- Setup vars, run updated containers -----"
 cd /MACBot/docker
 
+rm env-file
+
 # Update sources
 git pull origin master
 
+echo "TELEGRAM_TOKEN=$TELEGRAM_TOKEN" > env-file
+
 # Update containers with minimal downtime
-dockr-compose down
+docker-compose down
 docker-compose up --build -d
